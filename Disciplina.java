ca2b98b23dc4fbb54d31fc304d0013de137e3c62
fabricio1012 <128443331+fabricio1012@users.noreply.github.com>
2024-10-03 12:26:45 -0300

    Disciplina.java

---
+++ Disciplina.java
@@ -0,0 +1,28 @@
+public class Disciplina {
+    private String nome;
+    private int cargaHoraria;
+    private Professor professor;
+
+    public Disciplina(String nome, int cargaHoraria, Professor professor) {
+        this.nome = nome;
+        this.cargaHoraria = cargaHoraria;
+        this.professor = professor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public Professor getProfessor() {
+        return professor;
+    }
+
+    @Override
+    public String toString() {
+        return nome + " - " + cargaHoraria + " horas (Professor: " + professor.getNome() + ")";
+    }
+}
